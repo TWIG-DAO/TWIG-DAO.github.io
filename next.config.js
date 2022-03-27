2aeb1d41d5df1954afab6a1db15429ebd42f6b6a
iketiunn <ike.chang@sideeffect.dev>
2022-03-28 01:06:53 +0800

    ci: adding config for gh-page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/TWIG-DAO.github.io/' : '' // TODO: temp for github page
 }
 
 module.exports = nextConfig
